aa744fcf4b75dd0de2b21ea6c48c5d9adb244ed4
CHOI97 <chlwjrh@gmail.com>
2023-01-18 04:03:48 +0900

    [ feat ] 커스텀 다이얼로그 완료

---
+++ app/src/main/java/com/example/simya/CustomDialogInterface.kt
@@ -0,0 +1,6 @@
+package com.example.simya
+
+interface CustomDialogInterface {
+    fun testClickYes()
+    fun testClickNo()
+}
--- app/src/main/java/com/example/simya/MainActivity.kt
+++ app/src/main/java/com/example/simya/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.simya
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.simya.databinding.ActivityMainBinding
+import com.example.simya.databinding.DialogProfileBinding
+import com.example.simya.databinding.DialogProfileMasterBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewBinding: DialogProfileMasterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = DialogProfileMasterBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+//        viewBinding.btnTest.setOnClickListener{
+//            val myCustomDialog = MyCustomDialog(this,this)
+//            myCustomDialog.show()
+//        }
     }
+
+//    override fun testClickYes() {
+//        Toast.makeText(this, "예 클릭", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    override fun testClickNo() {
+//        Toast.makeText(this, "아니오 클릭", Toast.LENGTH_SHORT).show()
+//    }
 }
