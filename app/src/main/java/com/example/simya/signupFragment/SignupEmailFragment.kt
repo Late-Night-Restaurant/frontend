16ca0482ef6db65101556af2dfab72484c6bef46
아딘컴\김아린 <jyandysys1@naver.com>
2023-01-24 02:11:02 +0900

    [feat] : 회원가입

--- app/src/main/java/com/example/simya/signupFragment/SignupEmailFragment.kt
+++ app/src/main/java/com/example/simya/signupFragment/SignupEmailFragment.kt
@@ -1,14 +1,28 @@
 package com.example.simya.signupFragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.simya.databinding.ActivitySignupBinding
 import com.example.simya.databinding.FragmentSignupEmailBinding
 
 class SignupEmailFragment: Fragment() {
     private lateinit var binding: FragmentSignupEmailBinding
+    private lateinit var signupBinding: ActivitySignupBinding
+
+    lateinit var sendSignupListener: SendSignupListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        // Fragment가 Activity에 접근
+        sendSignupListener = context as SendSignupListener
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,10 +31,13 @@ class SignupEmailFragment: Fragment() {
     ): View? {
         binding = FragmentSignupEmailBinding.inflate(layoutInflater)
         return binding.root
+
+
     }
 
     // 다시 상속받는
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 }
