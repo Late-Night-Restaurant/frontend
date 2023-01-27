f5c21c0c36cef69593f1d93aad1077d2f97c2bf0
CHOI97 <chlwjrh@gmail.com>
2023-01-28 03:43:57 +0900

    [ feat ] 로그인 구현

--- app/src/main/java/com/example/simya/server/RetrofitService.kt
+++ app/src/main/java/com/example/simya/server/RetrofitService.kt
@@ -1,18 +1,20 @@
 package com.example.simya.server
 
+import com.example.simya.server.account.AccountDTO
 import com.example.simya.server.account.AccountResponse
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface RetrofitService {
     // 로그인 요청
     @Headers("Content-Type: application/json")
-    @GET("/simya/form-login")
+    @POST("/simya/form-login")
     fun onLoginSubmit(
-        @Query("email") email: String?,
-        @Query("password") password: String?
+        @Body login: AccountDTO
     ): Call<AccountResponse>
 
 
