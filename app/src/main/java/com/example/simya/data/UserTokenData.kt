5f260b14f5e2724de1d7bcdcd2d88e71dd072a42
CHOI97 <chlwjrh@gmail.com>
2023-02-01 03:44:18 +0900

    [ feat ] 채팅구현 끝

--- app/src/main/java/com/example/simya/data/UserTokenData.kt
+++ app/src/main/java/com/example/simya/data/UserTokenData.kt
@@ -3,24 +3,46 @@ package com.example.simya.data
 import com.example.simya.Constants
 import kotlin.properties.Delegates
 
-
+// 이름 바꿔야함
 object UserTokenData {
-    var accessToken : String = Constants.DEFAULT
+    var accessToken: String = Constants.DEFAULT
     var refreshToken: String = Constants.DEFAULT
-    fun init(accessToken: String, refreshToken: String){
+    private var profileId: Long = 0
+    private var profileName: String = Constants.DEFAULT
+    fun setProfileName(profileName: String) {
+        this.profileName = profileName
+    }
+
+    fun getProfileName(): String {
+        return profileName
+    }
+
+    fun setProfileId(profileId: Long) {
+        this.profileId = profileId
+    }
+
+    fun getProfileId(): Long {
+        return profileId
+    }
+
+    fun init(accessToken: String, refreshToken: String) {
         this.accessToken = "Access $accessToken"
         this.refreshToken = "Refresh $refreshToken"
     }
-    fun setUserAccessToken(accessToken: String){
+
+    fun setUserAccessToken(accessToken: String) {
         this.accessToken = accessToken
     }
-    fun setUserRefreshToken(refreshToken: String){
+
+    fun setUserRefreshToken(refreshToken: String) {
         this.refreshToken = refreshToken
     }
-    fun getUserAccessToken():String{
+
+    fun getUserAccessToken(): String {
         return accessToken
     }
-    fun getUserRefreshToken(): String{
+
+    fun getUserRefreshToken(): String {
         return refreshToken
     }
 
