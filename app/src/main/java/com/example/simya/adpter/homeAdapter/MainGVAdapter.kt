6adeca75d42b54d3629aad7da084e51f9c028057
CHOI97 <chlwjrh@gmail.com>
2023-01-31 09:42:42 +0900

    [ feat ] 이야기집 오픈 UI 업데이트 , 이야기집 ID전달 구현

--- app/src/main/java/com/example/simya/adpter/homeAdapter/MainGVAdapter.kt
+++ app/src/main/java/com/example/simya/adpter/homeAdapter/MainGVAdapter.kt
@@ -1,17 +1,22 @@
 package com.example.simya.adpter.homeAdapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.simya.adpter.mystoryAdapter.MyStoryGVAdater
 import com.example.simya.testData.TestDataBorder
 import com.example.simya.databinding.ItemBorderGv156156Binding
+import com.example.simya.server.story.LoadAllStoryResult
+import com.example.simya.server.story.LoadMyStoryResult
 
-class MainGVAdapter (private val dataList:ArrayList<TestDataBorder>): RecyclerView.Adapter<MainGVAdapter.DataViewHolder>() {
+class MainGVAdapter (private val dataList:ArrayList<LoadAllStoryResult>): RecyclerView.Adapter<MainGVAdapter.DataViewHolder>() {
+    private var listener: OnItemClickListener? = null
     inner class DataViewHolder(private val binding: ItemBorderGv156156Binding): RecyclerView.ViewHolder(binding.root){
-        fun bind(data: TestDataBorder) {
-            binding.tvGvTodayMenu.text = data.todayMenu
-            binding.tvGvMainMenu.text = data.mainMenu
-            binding.tvGvTitle.text = data.title
+        fun bind(data: LoadAllStoryResult) {
+            binding.tvGvTodayMenu.text = data.todayTopicTitle
+            binding.tvGvMainMenu.text = data.category
+            binding.tvGvTitle.text = data.houseName
         }
     }
     //ViewHolder 만들어 질때 실행할 동작
@@ -22,8 +27,23 @@ class MainGVAdapter (private val dataList:ArrayList<TestDataBorder>): RecyclerVi
     // ViewHolder가 실제로 데이터를 표시해야 할 때 호출되는 함수 , 데이터를 표시할때마다 호출
     override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
         holder.bind(dataList[position])
+        if(position != RecyclerView.NO_POSITION){
+            holder.itemView.setOnClickListener{
+                listener?.onItemClick(holder.itemView,dataList[position],position)
+            }
+        }
     }
     // 표현할 Item의 총 개수
-    override fun getItemCount(): Int= dataList.size
+    override fun getItemCount(): Int = dataList.size
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+    interface OnItemClickListener {
+        fun onItemClick(v: View, data: LoadAllStoryResult, position: Int)
+        fun onLongClick(v: View, data: LoadAllStoryResult, position: Int)
+    }
 
+    fun setOnItemClickListener(listener: OnItemClickListener){
+        this.listener = listener
+    }
 }
--- app/src/main/java/com/example/simya/server/RetrofitService.kt
+++ app/src/main/java/com/example/simya/server/RetrofitService.kt
@@ -56,6 +56,13 @@ interface RetrofitService {
         @Header("Refresh-Token") refreshToken: String
     ): Call<LoadMyStoryResponse>
 
+    @Headers("Content-Type: application/json")
+    @GET("/simya/house")
+    fun getAllStory(
+        @Header("Access-Token") accessToken: String,
+        @Header("Refresh-Token") refreshToken: String
+    ): Call<LoadAllStoryResponse>
+
     // 이야기 집 오픈
     @Headers("Content-Type: application/json")
     @PATCH("/simya/house/open")
