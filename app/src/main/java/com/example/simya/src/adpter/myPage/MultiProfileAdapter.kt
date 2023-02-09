d36eaad0bfc48228ae749f495e824fbe5492ddc9
CHOI97 <chlwjrh@gmail.com>
2023-02-10 01:40:55 +0900

    [ etc ] 마이페이지 멀티프로필 사이즈 및 이미지 파일 변경

--- app/src/main/java/com/example/simya/src/adpter/myPage/MultiProfileAdapter.kt
+++ app/src/main/java/com/example/simya/src/adpter/myPage/MultiProfileAdapter.kt
@@ -8,19 +8,20 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.simya.R
 import com.example.simya.databinding.ItemMultiProfileBinding
+import com.example.simya.databinding.ItemMypageMultiProfileBinding
 import com.example.simya.src.fragment.mypage.MyPageProfileFragment
 import com.example.simya.src.testData.TestDataMultiProfile
 
 class MultiProfileAdapter(private val context: Fragment, private val dataList: ArrayList<TestDataMultiProfile>) : RecyclerView.Adapter<MultiProfileAdapter.DataViewHolder>() {
-    inner class DataViewHolder(private val binding: ItemMultiProfileBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class DataViewHolder(private val binding: ItemMypageMultiProfileBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(data: TestDataMultiProfile) {
-            binding.tvMultiNickname.text = data.nickname
-            Glide.with(context).load(data.imageSource).into(binding.civItemMultiProfile)
+            binding.tvItemMyStoryMultiProfile.text = data.nickname
+            Glide.with(context).load(data.imageSource).into(binding.civItemMyStoryMultiProfile)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
-        val binding = ItemMultiProfileBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemMypageMultiProfileBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return DataViewHolder(binding)
     }
 
