5cc7bb7cce8dbf6db1315e4e20bf608a558dd33c
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-04-20 21:52:40 +0900

    Refactor: 회원가입 viewBinding -> dataBinding

--- app/src/main/java/com/example/simya/src/ui/view/login/signIn/EmailLoginActivity.kt
+++ app/src/main/java/com/example/simya/src/ui/view/login/signIn/EmailLoginActivity.kt
@@ -21,27 +21,15 @@ class EmailLoginActivity :
         viewModel = ViewModelProvider(this)[EmailLoginViewModel::class.java]
 
         binding.btnEmailSigninLogin.onThrottleClick {
-            viewModel.setEmailAndPassword(binding.edtEmailSignInInputEmail.text.toString(),binding.edtEmailSignInInputPassword.text.toString())
+            viewModel.setEmailAndPassword(
+                binding.edtEmailSignInInputEmail.text.toString(),
+                binding.edtEmailSignInInputPassword.text.toString()
+            )
             if (checkValidationEmail() && checkValidationPassword()) {
                 showLoadingDialog(this)
                 // 로그인 서비스
             }
         }
-
-//        Text Watcher 보류
-//        initTextWatcher()
-//                binding.edtEmailSignInInputEmail.addTextChangedListener(textWatcher)
-//        binding.edtEmailSignInInputPassword.addTextChangedListener(textWatcher)
-//        준비중인 서비스
-//        prePareServiceFindIdAndPassword()
-    }
-
-    private fun moveToHome() {
-//        val intent = Intent(this, HomeActivity::class.java)
-        // 메인,로그인 액티비티 스택 제거
-//        intent.flags = FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_CLEAR_TASK
-        finish()
-        startActivity(intent)
     }
 
     // 아이디 폼에 따른 Error 출력
@@ -54,6 +42,7 @@ class EmailLoginActivity :
             false
         }
     }
+
     // 비밀번호 폼에 따른 Error 출력
     private fun checkValidationPassword(): Boolean {
         return if (viewModel.checkPassword()) {
@@ -65,50 +54,6 @@ class EmailLoginActivity :
         }
     }
 
-//    private fun initTextWatcher() {
-//        textWatcher = object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-//            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-//                val emailInput = binding.edtEmailSignInInputEmail!!.text.toString()
-//                val passwordInput = binding.edtEmailSignInInputPassword!!.text.toString()
-//                if (emailInput.isNotEmpty() && passwordInput.isNotEmpty()) {
-//                    binding.btnEmailSigninLogin.isEnabled = true
-//                    binding.btnEmailSigninLogin.isClickable = true
-//                }
-//                if (emailInput.isEmpty() || passwordInput.isEmpty()) {
-//                    binding.btnEmailSigninLogin.isEnabled = false
-//                    binding.btnEmailSigninLogin.isClickable = false
-//                }
-//            }
-//
-//            override fun afterTextChanged(s: Editable) {
-//            }
-//        }
-//    }
-
-//    override fun onPostLoginSubmitSuccess(response: AccountResponse) {
-//        dismissLoadingDialog()
-//        LoginService(this).setAccessTokenSharedPreferences(response.result!!.accessToken)
-//        LoginService(this).setRefreshTokenSharedPreferences(response.result!!.refreshToken)
-//        UserData.setProfileId(response.result!!.profileId)
-//        UserData.setProfileName(response.result!!.nickname)
-//        UserData.setProfileComment(response.result!!.comment)
-//        UserData.setProfileImage(response.result!!.pictureUrl)
-//        UserData.setUserAccessToken(response.result!!.accessToken)
-//        UserData.setUserRefreshToken(response.result!!.refreshToken)
-//        UserData.printAllData()
-//        moveToHome()
-//    }
-//
-//    override fun onPostLoginSubmitFailure(response: BaseResponse) {
-//        dismissLoadingDialog()
-//        SampleSnackBar.make(binding.root, response.message.toString()).show()
-//    }
-//
-//    override fun onPostLoginSubmitDisconnect(message: String) {
-//        SampleSnackBar.make(binding.root, message).show()
-//        dismissLoadingDialog()
-//    }
 
     override fun onOKClicked() {
 
@@ -116,15 +61,5 @@ class EmailLoginActivity :
 
     override fun onBackPressed() {
         backApplicationExit(this)
-    }
-
-    // 아직 준비중인 서비스
-    private fun prePareServiceFindIdAndPassword() {
-        binding.btnSignInEmailFindEmail.onThrottleClick {
-            PrepareDialog(this, this).show()
-        }
-        binding.btnSignInEmailFindPassword.onThrottleClick {
-            PrepareDialog(this, this).show()
-        }
     }
 }
--- app/src/main/java/com/example/simya/src/ui/view/login/singup/SignupActivity.kt
+++ app/src/main/java/com/example/simya/src/ui/view/login/singup/SignupActivity.kt
@@ -1,62 +1,35 @@
-//package com.example.simya.src.ui.view.login.singup
-//
-//import android.os.Bundle
-//import com.example.simya.config.BaseActivity
-//import com.example.simya.databinding.ActivitySignupBinding
-//import com.example.simya.src.ui.view.login.singup.fragment.*
-//
-//class SignupActivity: BaseActivity<ActivitySignupBinding>(ActivitySignupBinding::inflate)
-//    {
-//    private var fragmentAgree = SignupAgreeFragment()
-//    private var fragmentEmail = SignupEmailFragment()
-//    private var fragmentPw = SignupPwFragment()
-//    private var fragmentProfile = SignupProfileFragment()
-//    private var fragmentFin = SignupFinFragment()
-//
-//    interface onBackPressedListener {
-//        fun onBackPressed()
-//    }
-//    override fun onBackPressed() {
-//        val fragmentList = supportFragmentManager.fragments
-//        for (fragment in fragmentList) {
-//            if (fragment is onBackPressedListener) {
-//                (fragment as onBackPressedListener).onBackPressed()
-//                return
-//            }
-//        }
-//    }
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        val view = binding.root
-//        setContentView(view)
-//        init()
-//    }
-//
-//    // count == 0 약관동의 fragment 표시
-//    private fun init() {
-//        val transaction = supportFragmentManager.beginTransaction()
-//            .replace(binding.fmSignup.id, fragmentAgree)
-//        transaction.commit()
-//        binding.pbSignup.progress = 0
-//    }
-//    fun increaseProgressbar(){
-//        binding.pbSignup.progress += 25
-//    }
-//
-//    fun decreaseProgressbar(){
-//        binding.pbSignup.progress -= 25
-//    }
-//
-//    fun nextFragmentSignUp(int: Int) {
-//        val transaction = supportFragmentManager.beginTransaction()
-//        when(int){
-//            1 -> transaction.replace(binding.fmSignup.id, fragmentAgree)
-//            2 -> transaction.replace(binding.fmSignup.id, fragmentEmail)
-//            3 -> transaction.replace(binding.fmSignup.id, fragmentPw)
-//            4 -> transaction.replace(binding.fmSignup.id, fragmentProfile)
-//            5 -> transaction.replace(binding.fmSignup.id, fragmentFin)
-//        }
-//        transaction.commit()
-//    }
-//
-//}
+package com.example.simya.src.ui.view.login.singup
+
+import android.os.Bundle
+import com.example.simya.R
+import com.example.simya.config.BaseActivity
+import com.example.simya.databinding.ActivitySignupBinding
+
+class SignupActivity : BaseActivity<ActivitySignupBinding>(R.layout.activity_signup) {
+
+    override fun init() {
+
+    }
+
+    interface onBackPressedListener {
+        fun onBackPressed()
+    }
+
+    override fun onBackPressed() {
+        val fragmentList = supportFragmentManager.fragments
+        for (fragment in fragmentList) {
+            if (fragment is onBackPressedListener) {
+                (fragment as onBackPressedListener).onBackPressed()
+                return
+            }
+        }
+    }
+    fun increaseProgressbar() {
+        binding.pbSignup.progress += 25
+    }
+
+    fun decreaseProgressbar() {
+        binding.pbSignup.progress -= 25
+    }
+
+}
