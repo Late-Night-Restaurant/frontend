ba9b7455409843c21e335ef5e3c096fbdbd7e99f
CHOI <32347874+CHOI97@users.noreply.github.com>
2023-05-01 14:41:17 +0900

    Refactor: 비밀번호 폼 UI수정, 2way Databinding 적용

--- app/src/main/java/com/example/simya/src/ui/view/login/signup/fragment/SignupPwFragment.kt
+++ app/src/main/java/com/example/simya/src/ui/view/login/signup/fragment/SignupPwFragment.kt
@@ -8,77 +8,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.*
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.example.simya.R
 import com.example.simya.config.BaseFragment
 import com.example.simya.src.ui.view.login.signup.SignupActivity
 import com.example.simya.databinding.FragmentSignupPwBinding
+import com.example.simya.src.ui.viewmodel.login.signup.SignupViewModel
 import com.example.simya.util.Constants.PW_VALIDATION
 import java.util.regex.Pattern
 
-class SignupPwFragment: BaseFragment<FragmentSignupPwBinding>(R.layout.fragment_signup_pw){
+class SignupPwFragment: BaseFragment<FragmentSignupPwBinding>(R.layout.fragment_signup_pw) {
 
-    var signupActivity: SignupActivity? = null
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        signupActivity = context as SignupActivity
-    }
+    private lateinit var signupViewModel: SignupViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        FalseButton()
+        signupViewModel = ViewModelProvider(activity as SignupActivity)[SignupViewModel::class.java]
+        binding.signupViewModel = signupViewModel
+
+        signupViewModel.pw.observe(viewLifecycleOwner,Observer{
+            pwEmpty()
+        })
+        signupViewModel.rePw.observe(viewLifecycleOwner,Observer{
+            pwEmpty()
+        })
         binding.btnSignupNextPw.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_signupPwFragment_to_signupProfileFragment)
+            Navigation.findNavController(view)
+                .navigate(R.id.action_signupPwFragment_to_signupProfileFragment)
+            signupViewModel.increaseProgress()
         }
     }
-
-
-    private fun pwCheck(): Boolean {
-        var pwLength = binding.tietPwSignupInput.text!!.length
-        var pw = binding.tietPwSignupInput.text.toString().trim()
-        val pattern = Pattern.matches(PW_VALIDATION, pw)
-
-        return if (pattern && pwLength in 8..12) {
-            binding.btnSignupNextPw.error = null
-            true
-        } else {
-            binding.tilPwSignupInput.error = "영문과 숫자를 조합해서 입력해주세요.(8-12자)"
-            false
-        }
+    private fun pwEmpty(){
+        binding.btnSignupNextPw.isEnabled = signupViewModel.pwEmptyCheck()
+        binding.btnSignupNextPw.isClickable = signupViewModel.pwEmptyCheck()
     }
-
-    private fun rePwCheck(): Boolean {
-        val rePw = binding.etRePwSignupInput.text.toString().trim()
-        val pw = binding.tietPwSignupInput.text.toString().trim()
-
-        return if (rePw == pw) {
-            binding.etRePwSignupInput.error = null
-            true
-        } else {
-            binding.etRePwSignupInput.error = "입력하신 비밀번호와 일치하지 않습니다."
-            false
-        }
-
-    }
-
-    private fun TrueButton() {
-        binding.btnSignupNextPw.isEnabled = true
-        binding.btnSignupNextPw.isClickable = true
-        binding.btnSignupNextPw.setBackgroundResource(R.drawable.low_radius_button_on)
-        binding.btnSignupNextPw.setTextColor(resources.getColor(R.color.Gray_03))
-
-    }
-
-    private fun FalseButton() {
-        binding.btnSignupNextPw.isEnabled = false
-        binding.btnSignupNextPw.isClickable = false
-        binding.btnSignupNextPw.setBackgroundResource(R.drawable.low_radius_button_off)
-        binding.btnSignupNextPw.setTextColor(resources.getColor(R.color.Gray_10))
-
-    }
-
-
-
 }
-
